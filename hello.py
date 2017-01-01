5104bb9ca85d2acd33121c479bdb0b0e1fc7dd2a
goo20161 <goo2016@gmail.com>
2017-01-01 22:12:28 +0800

    先搜索起点看最新章节，超过最新则不进行下一步的搜索；支持33yq.com和dhzw.com两个网站的搜索；如果章节找到多个，全部返回；搜索最新章节不用带0,直接输书名即可

--- hello.py
+++ hello.py
@@ -12,6 +12,108 @@ app = Flask(__name__)
 headers = {
     "User-Agent": "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.90 Safari/537.36"}
 
+CN_NUM = {
+    u'〇': 0,
+    u'一': 1,
+    u'二': 2,
+    u'三': 3,
+    u'四': 4,
+    u'五': 5,
+    u'六': 6,
+    u'七': 7,
+    u'八': 8,
+    u'九': 9,
+
+    u'零': 0,
+    u'壹': 1,
+    u'贰': 2,
+    u'叁': 3,
+    u'肆': 4,
+    u'伍': 5,
+    u'陆': 6,
+    u'柒': 7,
+    u'捌': 8,
+    u'玖': 9,
+
+    u'貮': 2,
+    u'两': 2,
+}
+CN_UNIT = {
+    u'十': 10,
+    u'拾': 10,
+    u'百': 100,
+    u'佰': 100,
+    u'千': 1000,
+    u'仟': 1000,
+    u'万': 10000,
+    u'萬': 10000,
+    u'亿': 100000000,
+    u'億': 100000000,
+    u'兆': 1000000000000,
+}
+
+
+def cn2dig(cn):
+    lcn = list(cn)
+    unit = 0  # 当前的单位
+    ldig = []  # 临时数组
+
+    while lcn:
+        cndig = lcn.pop()
+
+        if CN_UNIT.has_key(cndig):
+            unit = CN_UNIT.get(cndig)
+            if unit == 10000:
+                ldig.append('w')  # 标示万位
+                unit = 1
+            elif unit == 100000000:
+                ldig.append('y')  # 标示亿位
+                unit = 1
+            elif unit == 1000000000000:  # 标示兆位
+                ldig.append('z')
+                unit = 1
+
+            continue
+
+        else:
+            dig = CN_NUM.get(cndig)
+
+            if unit:
+                dig = dig * unit
+                unit = 0
+
+            ldig.append(dig)
+
+    if unit == 10:  # 处理10-19的数字
+        ldig.append(10)
+
+    ret = 0
+    tmp = 0
+
+    while ldig:
+        x = ldig.pop()
+
+        if x == 'w':
+            tmp *= 10000
+            ret += tmp
+            tmp = 0
+
+        elif x == 'y':
+            tmp *= 100000000
+            ret += tmp
+            tmp = 0
+
+        elif x == 'z':
+            tmp *= 1000000000000
+            ret += tmp
+            tmp = 0
+
+        else:
+            tmp += x
+
+    ret += tmp
+    return ret
+
 
 def changenum(num):
 
@@ -20,7 +122,7 @@ def changenum(num):
     num = cdata[0]
     if len(cdata) > 1:
         ckj = cdata[1]
-        print(ckj)
+        #print(ckj)
 
     dic_num = {
         "0": u"零",
@@ -48,7 +150,7 @@ def changenum(num):
     s = ""
     a = ""
     x = 0
-    print(len(str(num)) - 1)
+    #print(len(str(num)) - 1)
     index = len(str(num)) - 1
     for i in numList:
         if i == "0":
@@ -66,19 +168,47 @@ def changenum(num):
     return s
 
 
-@app.route('/', methods={'GET', 'POST'})
-def hello_world():
-    if request.method == 'POST':
-        search_string = request.form['bookname']
-        print search_string
-        if search_string == '':
-            return render_template('hello.html')
-        bookname, num = search_string.split(u' ')
+def searchQidian(bookname):
+    '''在qidian.com上搜索书籍，返回书信息页，最新章节序号
+        Args:
+            bookname: 书名
+        Returns:
+            book_url, latest_chapter: 书信息页，最新章节序号
+    '''
 
-        print urllib.quote(bookname.encode('utf-8'))
+    try:
+        book = urllib.quote(bookname.encode('utf-8'))
+        response = requests.get('http://se.qidian.com/?kw=' + book, timeout=10)
+        page = html.fromstring(response.text)
+        s = page.xpath('//*[@id="result-list"]/div/ul/li')[0]
+        title = s.xpath('.//h4')[0]
+        title_t = title.text_content()
+        title_a = title.xpath('.//a/@href')[0]
+        update = s.xpath('.//p[@class="update"]')[0].text_content()
 
-        # 搜索书籍
-        response = requests.get('http://www.33yq.com/')
+        if bookname in title_t:
+            m = re.search(u'第(\d*)([^\d]*)章', update)
+            if m is not None:
+                if m.groups()[1] == u'':
+                    return title_a, m.groups()[0]
+                return title_a, str(cn2dig(m.groups()[1]))
+        return None, None
+
+    except Exception, e:
+        print e.message
+        return None, None
+
+
+def search33yq(bookname, chapter_num):
+    '''在33yq.com上搜索书籍某一章，有则返回该章文字，无则返回None
+        Args:
+            bookname: 书名
+            chapter_num: 章节序号
+        Returns:
+            指定章节的文字
+    '''
+    try:
+        response = requests.get('http://www.33yq.com/', timeout=10)
         page = html.fromstring(response.content.decode('gbk'))
         s = page.xpath('//input[@name="s"]/@value')[0]
 
@@ -92,47 +222,135 @@ def hello_world():
         page = response.content.decode('utf-8')
         m = re.findall(r'www.33yq.com/read/\d+/\d+/', page)
         if len(m) == 0:
-            return render_template(
-                'hello.html',
-                input=search_string,
-                text=u'未找到书籍')
+            return None
 
         # 获取目录
         response = requests.get('http://' + m[0])
         page = html.fromstring(response.content.decode('gbk'))
         title = page.xpath('//title/text()')[0]
-        if bookname not in title:
-            return render_template(
-                'hello.html',
-                input=search_string,
-                text=u'未找到书籍')
+        if bookname != title:
+            return None
         bookdir = page.xpath('//div[@id="list"]//a')
 
-        if num == '0':
-            chapter_title = bookdir[-1].xpath('text()')[0]
-            response = requests.get(bookdir[-1].xpath('@href')[0])
-            page = html.fromstring(response.content.decode('gbk'))
-            content = page.xpath('//div[@class="zhangjieTXT"]/text()')
+        content = []
+        for a in bookdir:
+            chapter_title = a.xpath('text()')[0]
+            if u'第' + chapter_num + u'章' in chapter_title or u'第' + \
+                    changenum(chapter_num) + u'章' in chapter_title:
+                response = requests.get(a.xpath('@href')[0])
+                page = html.fromstring(response.content.decode('gbk'))
+                content.append(chapter_title)
+                content = content + page.xpath('//div[@class="zhangjieTXT"]/text()')
+
+        if content == []:
+            return None
+        return content
+    except Exception, e:
+        print '搜索33yq.com出现错误', e.message
+        return None
+
+
+def searchDhzw(bookname, chapter_num):
+    '''在dhzw.com上搜索书籍某一章，有则返回该章文字，无则返回None
+        Args:
+            bookname: 书名
+            chapter_num: 章节序号
+        Returns:
+            指定章节的文字
+    '''
+
+    try:
+        data = {'searchkey': bookname.encode('gbk')}
+        response = requests.post('http://www.dhzw.com/modules/article/search.php', headers=headers, data=data, timeout=10)
+        page = html.fromstring(response.content.decode('gbk'))
+
+        lis = page.xpath('//div[@class="l"]//li')
+        search_succ = False
+        for li in lis:
+            name = li.xpath('./*[@class="s2"]')[0].text_content()
+            name_url = li.xpath('./*[@class="s2"]/a/@href')[0]
+            latest = li.xpath('./*[@class="s3"]')[0].text_content()
+            if bookname == name:
+                search_succ = True
+                break
+
+        if not search_succ:
+            return None
+        # 获取目录
+        response = requests.get(name_url, timeout=10)
+        page = html.fromstring(response.content.decode('gbk'))
+        title = page.xpath('//title/text()')[0]
+        bookdir = page.xpath('//div[@id="list"]//a')
+
+        content = []
+        for a in bookdir:
+            chapter_title = a.xpath('text()')[0]
+            if u'第' + chapter_num + u'章' in chapter_title or u'第' + \
+                    changenum(chapter_num) + u'章' in chapter_title:
+                response = requests.get(name_url + a.xpath('@href')[0], timeout=10)
+                page = html.fromstring(response.content.decode('gbk'))
+                content.append(chapter_title)
+                content = content + page.xpath('//div[@id="BookText"]/text()')
+        if content == []:
+            return None
+        return content
+    except Exception, e:
+        print '搜索dhzw.com出现错误', e.message
+        return None
+
+
+def search23zw(bookname, chapter_num):
+    '''在23zw.com上搜索书籍某一章，有则返回该章文字，无则返回None
+        Args:
+            bookname: 书名
+            chapter_num: 章节序号
+        Returns:
+            指定章节的文字
+    '''
+    pass
+
+
+@app.route('/', methods={'GET', 'POST'})
+def hello_world():
+    if request.method == 'POST':
+        search_string = request.form['bookname']
+        print search_string
+        if search_string == '':
+            return render_template('hello.html')
+        words = search_string.split(u' ')
+        bookname = words[0]
+        chapter_num = '0'
+        if len(words) == 2:
+            chapter_num = words[1]
+
+        qidian_url, latest_chapter = searchQidian(bookname)
+        print 'qidian结果:', qidian_url, latest_chapter
+        if qidian_url is None:
+            return render_template('hello.html')
+        #print urllib.quote(bookname.encode('utf-8'))
+
+        if chapter_num == '0':
+            chapter_num = latest_chapter
+        elif int(chapter_num)>int(latest_chapter):
             return render_template(
                 'hello.html',
                 input=search_string,
-                chapter=chapter_title,
+                text=u'目前还没有该章节呢！')
+
+        content = search33yq(bookname, chapter_num)
+        if content is None:
+            content = searchDhzw(bookname, chapter_num)
+        if content is None:
+            return render_template(
+                'hello.html',
+                input=search_string,
+                text=u'未找到')
+        else:
+            return render_template(
+                'hello.html',
+                input=search_string,
                 text=content)
 
-        for a in bookdir:
-            chapter_title = a.xpath('text()')[0]
-            print chapter_title
-            if u'第' + num + u'章' in chapter_title or u'第' + \
-                    changenum(num) + u'章' in chapter_title:
-                response = requests.get(a.xpath('@href')[0])
-                page = html.fromstring(response.content.decode('gbk'))
-                content = page.xpath('//div[@class="zhangjieTXT"]/text()')
-                return render_template(
-                    'hello.html',
-                    input=search_string,
-                    chapter=chapter_title,
-                    text=content)
-        return render_template('hello.html', text=u'未找到章节')
     else:
         return render_template('hello.html')
 
